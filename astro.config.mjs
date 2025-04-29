c5926a386779c66e539c242b62f0237001684166
René Weiberlenn <rene.weiberlenn@live.de>
2025-04-29 20:26:12 +0200

    feat: setup deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://renespeaks.github.io',
+    base: 'resume',
     vite: {
         plugins: [tailwindcss()]
     }
