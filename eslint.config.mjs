a238a6d0a64c4f851e93b269b5df180f4a6501a4
René Weiberlenn <rene.weiberlenn@live.de>
2025-05-05 16:44:01 +0200

    chore: update eslint config
    feat: setup resume.json and corresponding schema validation
    
    update: tsconfig.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,36 @@
-import ts from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import astro from "eslint-plugin-astro";
-import astroParser from "astro-eslint-parser";
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import astro from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
 
-
-export default defineConfig([
-  {
-    files: ["**/*.js", "**/*.ts"],
-    plugins: {
-      "@typescript-eslint": ts,
+export default [
+    {
+        files: ['**/*.js', '**/*.ts'],
+        plugins: {
+            '@typescript-eslint': ts
+        },
+        languageOptions: {
+            parser: tsParser
+        },
+        rules: {
+            ...ts.configs['eslint-recommended'].overrides[0].rules,
+            ...ts.configs['recommended'].rules
+        }
     },
-    languageOptions: {
-      parser: tsParser,
-    },
-    rules: {
-      ...ts.configs["eslint-recommended"].overrides[0].rules,
-      ...ts.configs["recommended"].rules,
-    },
-  },
-  {
-    files: ["**/*.astro"],
-    plugins: {
-      astro: astro,
-    },
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: tsParser,
-        extraFileExtensions: [".astro"],
-      },
-    },
-    rules: {
-      ...astro.configs.recommended.rules,
-    },
-  },
-]);
+    {
+        files: ['**/*.astro'],
+        plugins: {
+            astro: astro
+        },
+        languageOptions: {
+            parser: astroParser,
+            parserOptions: {
+                parser: tsParser,
+                extraFileExtensions: ['.astro']
+            }
+        },
+        rules: {
+            ...astro.configs.recommended.rules
+        }
+    }
+];
---
+++ src/schemas/Resume.ts
@@ -0,0 +1,112 @@
+import { z } from 'zod';
+
+const profileSchema = z.object({
+    network: z.string(),
+    url: z.string().url()
+});
+
+export type Proifle = z.infer<typeof profileSchema>;
+
+const summaryObjectSchema = z.object({
+    title: z.string(),
+    content: z.string()
+});
+
+type SummaryObject = z.infer<typeof summaryObjectSchema>;
+
+const summarySchema = z.string().or(summaryObjectSchema);
+
+export type Summary = z.infer<typeof summarySchema>;
+
+export function isSummaryObject(summary: Summary): summary is SummaryObject {
+    return typeof summary === 'object' && 'title' in summary && 'content' in summary;
+}
+
+const basicsSchema = z.object({
+    name: z.string(),
+    label: z.string(),
+    summary: z.array(summarySchema),
+    image: z.optional(z.string()),
+    url: z.optional(z.string().url()),
+    profiles: z.optional(z.array(profileSchema))
+});
+
+export type Basics = z.infer<typeof basicsSchema>;
+
+const dateStringSchema = z
+    .string()
+    .regex(/^(January|February|March|April|May|June|July|August|September|October|November|December) \d{4}$/);
+
+const certificateSchema = z.object({
+    name: z.string(),
+    issuer: z.string(),
+    startDate: dateStringSchema,
+    url: z.optional(z.string().url())
+});
+
+export type Certificate = z.infer<typeof certificateSchema>;
+
+const educationSchema = z.object({
+    area: z.string(),
+    institution: z.string(),
+    studyType: z.string(),
+    startDate: dateStringSchema,
+    endDate: z.optional(dateStringSchema),
+    summary: z.optional(z.string()),
+    score: z.optional(z.string())
+});
+
+export type Education = z.infer<typeof educationSchema>;
+
+const languageSchema = z.object({
+    language: z.string(),
+    fluency: z.enum(['A', 'B', 'C', 'native'])
+});
+
+export type Language = z.infer<typeof languageSchema>;
+
+const projectSchema = z.object({
+    name: z.string(),
+    description: z.string(),
+    highlights: z.optional(z.array(z.string())),
+    startDate: dateStringSchema,
+    endDate: z.optional(dateStringSchema),
+    skills: z.optional(z.array(z.string())),
+    url: z.optional(z.string().url())
+});
+
+export type Project = z.infer<typeof projectSchema>;
+
+const skillSchema = z.object({
+    name: z.string(),
+    level: z.enum(['beginner', 'intermediate', 'advanced', 'expert'])
+});
+
+export type Skill = z.infer<typeof skillSchema>;
+
+const workSchema = z.object({
+    name: z.string(),
+    position: z.string(),
+    summary: z.string(),
+    startDate: dateStringSchema,
+    endDate: z.optional(dateStringSchema),
+    highlights: z.optional(z.array(z.string())),
+    location: z.optional(z.string()),
+    projects: z.optional(z.array(z.string())),
+    skills: z.optional(z.array(z.string())),
+    url: z.optional(z.string().url())
+});
+
+export type Work = z.infer<typeof workSchema>;
+
+export const resumeSchema = z.object({
+    basics: basicsSchema,
+    certificates: z.optional(z.array(certificateSchema)),
+    education: z.optional(z.array(educationSchema)),
+    languages: z.optional(z.array(languageSchema)),
+    projects: z.optional(z.array(projectSchema)),
+    skills: z.optional(z.array(skillSchema)),
+    work: z.optional(z.array(workSchema))
+});
+
+export type Resume = z.infer<typeof resumeSchema>;
